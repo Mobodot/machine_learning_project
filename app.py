43c0fbc1ac5b92fd92da15233b9516de75b94e59
Mobodot <mobosomto@gmail.com>
2022-06-12 08:40:06 +0100

    edited the app.py file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Starting Machine Learning Project"
+    return "Starting this content should deploy automatically no??"
 
 
 if __name__ == "__main__":
