84aa0e4bc577f5ee99b7638efe73aac97c898de5
Mobodot <mobosomto@gmail.com>
2022-06-24 08:27:14 +0100

    created the structure of the project

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from crypt import methods
+import sys
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    try:
+        raise Exception("We are test custom exception")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return "Starting this content should deploy automatically no??"
 
 
