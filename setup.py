02204e106f9691404a31b6e8d7c0143e051dd450
Mobodot <mobosomto@gmail.com>
2022-07-10 10:34:34 +0100

    project completed

--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@ from setuptools import setup, find_packages
 from typing import List
 
 
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
 def get_requirements_list() -> List[str]:
     """
-    Description: this function will return list of requirements
+    Description: This function is going to return list of requirement
     mention in requirements.txt file
-
-    :return: This function is going to return a list which contain names
-    of libraries  mentioned in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
     """
-    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readlines().remove("-e .")
-
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 # declaring variables ofr setup function
 PROJECT_NAME = "housing-predictor"
